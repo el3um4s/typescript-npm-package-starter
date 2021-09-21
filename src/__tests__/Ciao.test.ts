fa778cfb179e704bcd8ab69bdafd14ef4120b1c4
Samuele de Tomasi <el3um4s@gmail.com>
2021-09-21 18:59:16 +0200

    Added JEST

---
+++ src/__tests__/Ciao.test.ts
@@ -0,0 +1,8 @@
+import { myCustomFunction, ciao } from '../index';
+test('ciao', () => {
+  expect(ciao('Mondo')).toBe('Ciao Mondo');
+});
+
+test("myCustomFunction", () => {
+  expect(myCustomFunction('Mario')).toBe('Hello Mario');
+})
